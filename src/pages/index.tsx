b36cc234b9c4f87eeb4dbff565610d3e00c71c21
= <alexandressoares383@gmail.com>
2022-02-18 11:33:46 -0300

    feat: creating queries

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
+import { usePageQuery } from "../generated/graphql";
+
 export default function Home() {
+  const [{ data }] = usePageQuery({
+    variables: {
+      slug: "home"
+    }
+  });
+  
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>{data?.page.title}</h1>
+      <p>{data?.page.subtitle}</p>
+      <small>{data?.page.seo.title}</small>
+    </>
   )
 }
