7b14088d50cbd609819e7cd4cf3ae1ec1fc8ac8e
= <alexandressoares383@gmail.com>
2022-02-18 12:23:33 -0300

    feat: creating post queries

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-import { usePageQuery } from "../generated/graphql";
+import { GetServerSideProps } from "next";
+import { PageDocument, usePageQuery } from "../generated/graphql";
+import { client, ssrCache } from "../lib/urql";
 
 export default function Home() {
   const [{ data }] = usePageQuery({
@@ -14,4 +16,14 @@ export default function Home() {
       <small>{data?.page.seo.title}</small>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  await client.query(PageDocument, { slug: 'home' }).toPromise();
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData()
+    }
+  }
 }
